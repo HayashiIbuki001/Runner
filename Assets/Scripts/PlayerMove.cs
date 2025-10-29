1242461832776caab0dbdc8547b4f95e18d10a7f
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2025-10-29 19:33:23 +0900

    プレイヤーの挙動

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField,Tooltip("ƒWƒƒƒ“ƒvˆÐ—Í")] private float jumpForce = 100f;
+    [SerializeField, Tooltip("—Ž‰ºˆÐ—Í")] private float flowForce = 100f;
+
+    private Rigidbody2D rb;
+
+    /// <summary> ’…’n”»’è </summary>
+    private bool isGround;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (isGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            isGround = false;
+        }
+
+        if (!isGround && Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, -flowForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+}
